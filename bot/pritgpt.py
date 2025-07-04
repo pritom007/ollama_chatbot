e45bd4d3ccf6cc51245f7ad4ae563592c746d967
Pritom <pritom007@live.com>
2025-07-04 10:33:44 +0400

    Update pritgpt.py

--- bot/pritgpt.py
+++ bot/pritgpt.py
@@ -11,7 +11,7 @@ client = Groq(
 
 headers = {
     'Content-Type': 'application/json',
-    'api-key': os.environ.get('SAFU_GPT_API_KEY'),
+    'api-key': '',
 }
 
 params = {
@@ -37,35 +37,10 @@ def ollama_response(messages, temperature=0.7, model="llama3-8b-8912"):
             model='llama3-8b-8192',
             temperature=temperature,
         )
-    
-
-
-def safugpt_response(messages, max_tokens=800, temperature=0.7, top_p=0.95, stop=None):
-    json_data = {
-        'messages': messages,
-        'max_tokens': max_tokens,
-        'temperature': temperature,
-        'top_p': 0.95,
-        'stop': None,
-    }
-
-    json_response = requests.post(
-        'https://xyz.dev.xxz.net/openai/deployments/xyz/chat/completions',
-        params=params,
-        headers=headers,
-        json=json_data,
-    ).json()
-
-    # Convert JSON response to custom class instances
-    choices = [Choice(Message(choice['message']['content'])) for choice in json_response['choices']]
-    return  Response(choices)
-
 
 
 def qagpt_response(messages, max_tokens=800, temperature=0.7, top_p=0.95, stop=None, model='llama3-8b-8912', type='ollama'):
     if type == 'ollama':
         return ollama_response(messages, temperature, model)
-    elif type == 'safugpt':
-        return safugpt_response(messages, max_tokens, temperature, top_p, stop)
     else:
         raise ValueError(f"Unknown model type: {type}")
